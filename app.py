612743e7a334561d46ed88c6cc066fafbdfa753f
jihye13621 <jihye13621@yahoo.com>
2018-01-28 15:02:48 -0800

    cleaned up server routes

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import json
-import VisualGenerator
-import imageStuff
 from flask import Flask, request, url_for, redirect, render_template, abort, send_from_directory
 
 app = Flask(__name__)
@@ -10,10 +8,8 @@ midi = midiStuff.midiStuff()
 
 @app.route('/')
 def index():
-    pathToImage = "dog.jpg"
-    img.path = pathToImage
-    img.showImage(name="original")
-    return vis.whoAmI()
+
+    return "index"
 
 @app.route('/prettyjson')
 def prettyjson():
@@ -33,11 +29,5 @@ def prettyjson():
 def upload():
     return "Here is the upload page"
 
-@app.route('/visual/<modifiedName>')
-def visual(modifiedName=None):
-    # if nothing is passed, it is ok, default to none
-    vis.modifyName(modifiedName)
-    return "My name has changed to "+ vis.whoAmI()
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
